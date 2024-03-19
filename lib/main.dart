87c84a25e96c193f0ab009076e124174f4ded48f
AZIZ <ziz8fhad@gmail.com>
2024-03-20 00:24:34 +0300

    Four

--- lib/main.dart
+++ lib/main.dart
@@ -37,179 +37,231 @@ class _MoneyAppState extends State<MoneyApp> {
       ),
       body: Column(
         children: [
-          Column(
-            children: [
-              Row(
-                children: [Text("Enter Salary: "),_SalaryField(), Text("SAR")],
-              ), // widdgets folder
-              Row(
-                children: [_Choices()],
-              ) //Choices
-            ],
+          Expanded(
+            child: Column(
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                            child: Expanded(
+                          child: Row(children: [
+                            Text("Enter Salary: "),
+                            _SalaryField(),
+                            Text("SAR")
+                          ]),
+                        )),
+                      )
+                    ],
+                  ),
+                ), // widdgets folder
+                _Choices()
+              ],
+            ),
           ),
-          Column(children: [
-            if (isFoodTrue)
-              Row(children: [
-                Row(
-                  children: [Text("Food")],
-                ),
-                Column(
-                  children: [
-                    Row(children: [Text("Value %")]),
-                    Row(children: [Text("Data")]),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Row(children: [Text("Percentage %")]),
-                    Row(children: [Text("Data")]),
-                  ],
-                )
-              ]),
-            if (isSavingTrue)
-              Row(children: [
-                Row(
-                  children: [Text("Food")],
-                ),
-                Column(
-                  children: [
-                    Row(children: [Text("Value %")]),
-                    Row(children: [Text("Data")]),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Row(children: [Text("Percentage %")]),
-                    Row(children: [Text("Data")]),
-                  ],
-                )
-              ]),
-            if (isInvestTrue)
-              Row(children: [
-                Row(
-                  children: [Text("Food")],
-                ),
-                Column(
-                  children: [
-                    Row(children: [Text("Value %")]),
-                    Row(children: [Text("Data")]),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Row(children: [Text("Percentage %")]),
-                    Row(children: [Text("Data")]),
-                  ],
-                )
-              ]),
-            if (isEntertainmentTrue)
-              Row(children: [
-                Row(
-                  children: [Text("Food")],
-                ),
-                Column(
-                  children: [
-                    Row(children: [Text("Value %")]),
-                    Row(children: [Text("Data")]),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Row(children: [Text("Percentage %")]),
-                    Row(children: [Text("Data")]),
-                  ],
-                )
-              ]),
-            if (isBillsTrue)
-              Row(children: [
-                Row(
-                  children: [Text("Food")],
-                ),
-                Column(
-                  children: [
-                    Row(children: [Text("Value %")]),
-                    Row(children: [Text("Data")]),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Row(children: [Text("Percentage %")]),
-                    Row(children: [Text("Data")]),
-                  ],
-                )
-              ]),
-          ])
+          Expanded(
+            child: Column(children: [
+              if (isFoodTrue)
+                Row(children: [
+                  Expanded(
+                    child: Row(
+                      children: [Text("Food")],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Row(children: [Text("Value %")]),
+                        Row(children: [Text("Data")]),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Row(children: [Text("Percentage %")]),
+                        Row(children: [Text("Data")]),
+                      ],
+                    ),
+                  )
+                ]),
+              if (isSavingTrue)
+                Row(children: [
+                  Expanded(
+                    child: Row(
+                      children: [Text("Food")],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Row(children: [Text("Value %")]),
+                        Row(children: [Text("Data")]),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Row(children: [Text("Percentage %")]),
+                        Row(children: [Text("Data")]),
+                      ],
+                    ),
+                  )
+                ]),
+              if (isInvestTrue)
+                Row(children: [
+                  Expanded(
+                    child: Row(
+                      children: [Text("Food")],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Row(children: [Text("Value %")]),
+                        Row(children: [Text("Data")]),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Row(children: [Text("Percentage %")]),
+                        Row(children: [Text("Data")]),
+                      ],
+                    ),
+                  )
+                ]),
+              if (isEntertainmentTrue)
+                Row(children: [
+                  Expanded(
+                    child: Row(
+                      children: [Text("Food")],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Row(children: [Text("Value %")]),
+                        Row(children: [Text("Data")]),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Row(children: [Text("Percentage %")]),
+                        Row(children: [Text("Data")]),
+                      ],
+                    ),
+                  )
+                ]),
+              if (isBillsTrue)
+                Row(children: [
+                  Expanded(
+                    child: Row(
+                      children: [Text("Food")],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Row(children: [Text("Value %")]),
+                        Row(children: [Text("Data")]),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Row(children: [Text("Percentage %")]),
+                        Row(children: [Text("Data")]),
+                      ],
+                    ),
+                  )
+                ]),
+            ]),
+          ),
         ],
       ),
     );
   }
 
   Widget _SalaryField() {
-
-    setState(() {
-      
-    });
-    return TextField();
+    setState(() {});
+    return Expanded(child: TextField());
   }
 
   Widget _Choices() {
     return Container(
         child: Row(
       children: [
-        Column(
-          children: [
-            const Text("Food"),
-            Checkbox(
-                value: isFoodTrue,
-                onChanged: (value) {
-                  isFoodTrue = value!;
-                  setState(() {});
-                })
-          ],
+        Expanded(
+          child: Column(
+            children: [
+              const Text("Food"),
+              Checkbox(
+                  value: isFoodTrue,
+                  onChanged: (value) {
+                    isFoodTrue = value!;
+                    setState(() {});
+                  })
+            ],
+          ),
         ),
-        Column(
-          children: [
-            const Text("Entertainment"),
-            Checkbox(
-                value: isEntertainmentTrue,
-                onChanged: (value) {
-                  isEntertainmentTrue = value!;
-                  setState(() {});
-                })
-          ],
+        Expanded(
+          child: Column(
+            children: [
+              const Text("Entertainment"),
+              Checkbox(
+                  value: isEntertainmentTrue,
+                  onChanged: (value) {
+                    isEntertainmentTrue = value!;
+                    setState(() {});
+                  })
+            ],
+          ),
         ),
-        Column(
-          children: [
-            const Text("Investment"),
-            Checkbox(
-                value: isInvestTrue,
-                onChanged: (value) {
-                  isInvestTrue = value!;
-                  setState(() {});
-                })
-          ],
+        Expanded(
+          child: Column(
+            children: [
+              const Text("Investment"),
+              Checkbox(
+                  value: isInvestTrue,
+                  onChanged: (value) {
+                    isInvestTrue = value!;
+                    setState(() {});
+                  })
+            ],
+          ),
         ),
-        Column(
-          children: [
-            const Text("Bills"),
-            Checkbox(
-                value: isBillsTrue,
-                onChanged: (value) {
-                  isBillsTrue = value!;
-                  setState(() {});
-                })
-          ],
+        Expanded(
+          child: Column(
+            children: [
+              const Text("Bills"),
+              Checkbox(
+                  value: isBillsTrue,
+                  onChanged: (value) {
+                    isBillsTrue = value!;
+                    setState(() {});
+                  })
+            ],
+          ),
         ),
-        Column(
-          children: [
-            const Text("Saving"),
-            Checkbox(
-                value: isSavingTrue,
-                onChanged: (value) {
-                  isSavingTrue = value!;
-                  setState(() {});
-                })
-          ],
+        Expanded(
+          child: Column(
+            children: [
+              const Text("Saving"),
+              Checkbox(
+                  value: isSavingTrue,
+                  onChanged: (value) {
+                    isSavingTrue = value!;
+                    setState(() {});
+                  })
+            ],
+          ),
         ),
       ],
     ));
