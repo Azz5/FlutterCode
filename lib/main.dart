c119be60d0e6d936202f5c9e916d08dcd654f5ad
AZIZ <ziz8fhad@gmail.com>
2024-03-19 23:27:56 +0300

    TWO

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import "package:flutter/widgets.dart";
 
@@ -16,19 +17,134 @@ class _MoneyAppState extends State<MoneyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Row(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Row(
+          children: [
+            Icon(Icons.money),
+            Text(
+              "Money Manager",
+              textDirection: TextDirection.ltr,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Column(
             children: [
-              Icon(Icons.money),
-              Text(
-                "Money Manager",
-                textDirection: TextDirection.ltr,
-                textAlign: TextAlign.center,
-              ),
+              Row(
+                children: [_SalaryField()],
+              ), // widdgets folder
+              Row(
+                children: [_Choices()],
+              ) //Choices
             ],
           ),
-        ),
-        body: Column());
+          Column(children: [
+            if (/*_Choices().isFoodTrue*/ true)
+              Row(children: [
+                Row(
+                  children: [Text("Food")],
+                ),
+                Column(
+                  children: [
+                    Row(children: [Text("Value %")]),
+                    Row(children: [Text("Data")]),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Row(children: [Text("Percentage %")]),
+                    Row(children: [Text("Data")]),
+                  ],
+                )
+              ]),
+            if (/*_Choices().isSavingTrue*/ true)
+              Row(children: [
+                Row(
+                  children: [Text("Food")],
+                ),
+                Column(
+                  children: [
+                    Row(children: [Text("Value %")]),
+                    Row(children: [Text("Data")]),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Row(children: [Text("Percentage %")]),
+                    Row(children: [Text("Data")]),
+                  ],
+                )
+              ]),
+            if (/*_Choices().isInvestTrue*/ true)
+              Row(children: [
+                Row(
+                  children: [Text("Food")],
+                ),
+                Column(
+                  children: [
+                    Row(children: [Text("Value %")]),
+                    Row(children: [Text("Data")]),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Row(children: [Text("Percentage %")]),
+                    Row(children: [Text("Data")]),
+                  ],
+                )
+              ]),
+            if (/*_Choices().isEntertainmentTrue*/ true)
+              Row(children: [
+                Row(
+                  children: [Text("Food")],
+                ),
+                Column(
+                  children: [
+                    Row(children: [Text("Value %")]),
+                    Row(children: [Text("Data")]),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Row(children: [Text("Percentage %")]),
+                    Row(children: [Text("Data")]),
+                  ],
+                )
+              ]),
+            if (/*_Choices().isBillsTrue*/ true)
+              Row(children: [
+                Row(
+                  children: [Text("Food")],
+                ),
+                Column(
+                  children: [
+                    Row(children: [Text("Value %")]),
+                    Row(children: [Text("Data")]),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Row(children: [Text("Percentage %")]),
+                    Row(children: [Text("Data")]),
+                  ],
+                )
+              ]),
+          ])
+        ],
+      ),
+    );
+  }
+
+  Widget _SalaryField() {
+    setState(() {});
+    return TextField();
+  }
+
+  Widget _Choices() {
+    return Container();
   }
 }
